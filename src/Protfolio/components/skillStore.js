c6f0a6d737b503950e307511e4481255c708b612
Jahongir7 <Jahongir95119@gmail.com>
2021-11-17 20:31:12 +0500

    skillbar changed

--- src/Protfolio/components/skillStore.js
+++ src/Protfolio/components/skillStore.js
@@ -1,69 +1,59 @@
-import python from '../images/python.svg'
-import js from '../images/js.svg'
-import react from '../images/react.svg'
-
+import js from "../images/js.svg";
+import react from "../images/react.svg";
 
 var skill_language_store = [
-    {
-        name: 'Python',
-        level: 'verygood',
-        logo: python
-    },
-    {
-        name: 'Django',
-        level: 'avg',
-        logo: ''
-    },
-    {
-        name: 'Java Script',
-        level: 'verygood',
-        logo: js
-    },
-    {
-        name: 'React',
-        level: 'avg',
-        logo: react
-    },
-    {
-        name: 'Node js',
-        level: 'beg',
-        logo: ''
-    },
-    {
-        name: 'HTML & CSS',
-        level: 'exp',
-        logo: ''
-    },
-    {
-        name: 'bootstrap & Material UI',
-        level: 'exp',
-        logo: ''
-    },
-    {
-        name: 'SQL',
-        level: 'beg',
-        logo: ''
-    }
-]
+  {
+    name: "Java Script",
+    level: "verygood",
+    logo: js,
+  },
+  {
+    name: "React",
+    level: "exp",
+    logo: react,
+  },
+  {
+    name: "Node js",
+    level: "beg",
+    logo: "",
+  },
+  {
+    name: "HTML & CSS",
+    level: "exp",
+    logo: "",
+  },
+  {
+    name: "bootstrap & Material UI",
+    level: "exp",
+    logo: "",
+  },
+  {
+    name: "SQL",
+    level: "beg",
+    logo: "",
+  },
+];
 var skill_tool_store = [
-    {
-        name: 'git',
-        level: 'avg',
-        logo: ''
-    },
-    {
-        name: 'Figma',
-        level: 'verygood',
-        logo: ''
-    }, {
-        name: 'Photoshop',
-        level: 'avg',
-        logo: ''
-    }, {
-        name: 'Illustrator',
-        level: 'beg',
-        logo: ''
-    }
-]
+  {
+    name: "git",
+    level: "avg",
+    logo: "",
+  },
+  {
+    name: "Figma",
+    level: "verygood",
+    logo: "",
+  },
+  {
+    name: "Photoshop",
+    level: "avg",
+    logo: "",
+  },
+  {
+    name: "Illustrator",
+    level: "beg",
+    logo: "",
+  },
+];
 
-export { skill_language_store, skill_tool_store }
+export { skill_language_store, skill_tool_store };
